95716bd7c11e37fe1fc460b945cd85f7d96c2361
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-07-05 03:05:00 +0900

    투데이 뷰 완성 (상세정보 빼고)

--- AppStore_Study/Scenes/Today/TodayCollectionViewCell.swift
+++ AppStore_Study/Scenes/Today/TodayCollectionViewCell.swift
@@ -9,7 +9,84 @@ import SnapKit
 import UIKit
 
 final class TodayCollectionViewCell: UICollectionViewCell {
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 24.0, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        // 이미지 뷰보다 이미지가 크면 이미지가 이미지뷰 벗어나서 표시되는거 방지
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 12.0
+        imageView.backgroundColor = .gray
+        
+        return imageView
+    }()
+    
+    // MARK: 셀 안에 뜨는 것
     func setup() {
-        backgroundColor = .black
+        setUpSubViews()
+        
+        // MARK: 그림자 생성
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.3
+        layer.shadowRadius = 10
+        
+        subTitleLabel.text = "서브타이틀"
+        descriptionLabel.text = "설명설명"
+        titleLabel.text = "앱의 이름"
+    }
+}
+
+// 레이아웃
+private extension TodayCollectionViewCell {
+    func setUpSubViews() {
+        // 컴포넌트들 다 addSubView
+        [imageView, titleLabel, subTitleLabel, descriptionLabel]
+            .forEach{addSubview($0)}
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24.0)
+            $0.trailing.equalToSuperview().inset(24.0)
+            $0.top.equalToSuperview().inset(24.0)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(subTitleLabel)
+            $0.trailing.equalTo(subTitleLabel)
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(4.0)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24.0)
+            $0.trailing.equalToSuperview().inset(24.0)
+            $0.bottom.equalToSuperview().inset(24.0)
+        }
+        
+        // 이미지뷰를 상단뷰에 딱 맞춤
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
--- AppStore_Study/Scenes/Today/TodayViewController.swift
+++ AppStore_Study/Scenes/Today/TodayViewController.swift
@@ -26,6 +26,9 @@ final class TodayViewController: UIViewController {
         collectionView.backgroundColor = .systemBackground
         // MARK: 셀을 넣음
         collectionView.register(TodayCollectionViewCell.self, forCellWithReuseIdentifier: "todayCell")
+        // 레지스터 구현을 해줘야 컬렉션 뷰에서 가져와서 사용 가능
+        // 푸터는 젤 밑에
+        collectionView.register(TodayCollectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "TodayCollectionHeaderView")
         
         return collectionView
     }()
@@ -40,22 +43,6 @@ final class TodayViewController: UIViewController {
     }
 }
 
-extension TodayViewController: UICollectionViewDataSource {
-    // 셀을 리턴
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // as? 는 TodayCollectionViewCell 캐스팅
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "todayCell", for: indexPath) as? TodayCollectionViewCell
-        cell?.setup()
-
-        return cell ?? UICollectionViewCell()
-    }
-
-    // 셀을 몇개 쓸건지, 기본으로 셀 하나는 디폴트
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        5
-    }
-}
-
 // 컬렉션뷰의 셀 사이즈가 리턴되는 값을 기준으로 설정됌
 extension TodayViewController: UICollectionViewDelegateFlowLayout {
     // 셀의 사이즈
@@ -65,6 +52,51 @@ extension TodayViewController: UICollectionViewDelegateFlowLayout {
         let width = collectionView.frame.width - 32
 
         return CGSize(width: width, height: width)
+    }
+    
+    // 헤더의 사이즈
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        CGSize(width: collectionView.frame.width - 32.0, height: 100)
+    }
+    
+    // 헤더 위치
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let value:CGFloat = 16.0
+        return UIEdgeInsets(top: value, left: value, bottom: value, right: value)
+    }
+}
+
+extension TodayViewController: UICollectionViewDataSource {
+    // 셀을 몇개 쓸건지, 기본으로 셀 하나는 디폴트
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    // 셀을 리턴
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // as? 는 TodayCollectionViewCell 캐스팅
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "todayCell", for: indexPath) as? TodayCollectionViewCell
+        cell?.setup()
+
+        return cell ?? UICollectionViewCell()
+    }
+    
+    // viewForSupplementaryElementOfKind얘는 푸터일때도 불리고 헤더일때도 불리니까 guard로 헤더일때만 필터링을 함
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        // MARK: 헤더 푸터 구분
+        // 헤더면 출력하고 아니면 기본값 리턴
+        guard
+            kind == UICollectionView.elementKindSectionHeader,
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "TodayCollectionHeaderView", for: indexPath) as? TodayCollectionHeaderView
+        else {
+            return UICollectionReusableView()
+        }
+        
+        // 레이아웃 설정
+        // 헤더면 실행됨
+        header.setupViews()
+        
+        return header
     }
 }
 
